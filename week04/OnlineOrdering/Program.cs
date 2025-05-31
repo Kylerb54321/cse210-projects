c38748c6c6b0bf7ae401dc43ed4b3659998f956e
Kyle Bailey <bailey.k.r.12@gmail.com>
2025-05-31 16:49:46 +0200

    online

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -5,7 +5,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        // First Order
         Address address1 = new Address("123 Main St", "New York", "NY", "USA");
         Customer customer1 = new Customer("John Smith", address1);
         Order order1 = new Order(customer1);
@@ -18,7 +17,7 @@ class Program
         Console.WriteLine("Total Price: $" + order1.GetTotalCost());
         Console.WriteLine();
 
-        // Second Order
+        
         Address address2 = new Address("456 Queen St", "Toronto", "ON", "Canada");
         Customer customer2 = new Customer("Jane Doe", address2);
         Order order2 = new Order(customer2);
